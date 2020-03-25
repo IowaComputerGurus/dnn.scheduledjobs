0ea09d51a6dd9a8ee3718b5cdffc4ff8f00f62b4
Mitchel Sellers <msellers@iowacomputergurus.com>
2020-03-25 11:37:36 -0500

    Standardization of the project for coding standards as well as license agreements

--- ScheduledJobs/Components/JobRunnerTask.cs
+++ ScheduledJobs/Components/JobRunnerTask.cs
@@ -1,25 +1,7 @@
-/*
- * Copyright (c) 2008-2010 IowaComputerGurus Inc (http://www.iowacomputergurus.com)
- * Copyright Contact: webmaster@iowacomputergurus.com
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to deal 
- * in the Software without restriction, including without limitation the rights to use, 
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
- * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial 
- * portions of the Software. 
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
- * */
+// IowaComputerGurus, Inc. licenses this file to you under the MIT License
+// See the LICENSE file in the project root for more information
 
 using System;
-using System.Collections.Generic;
 using DotNetNuke.Services.Exceptions;
 using DotNetNuke.Services.Scheduling;
 
@@ -68,27 +50,27 @@ namespace ICG.Modules.ScheduledSqlJobs.Components
         public void RunJobs()
         {
             var oController = new ScheduledSqlJobsController();
-            List<JobScheduleInfo> oJobs = oController.GetJobScheduleItemsToRun();
+            var oJobs = oController.GetJobScheduleItemsToRun();
 
             if (oJobs != null && oJobs.Count > 0)
             {
-                InsertLogNote("A total of " + oJobs.Count.ToString() + " jobs to process");
+                InsertLogNote($"A total of {oJobs.Count} jobs to process");
                 foreach (JobScheduleInfo oCurrentJob in oJobs)
                 {
-                    InsertLogNote("Processing " + oCurrentJob.JobTitle);
+                    InsertLogNote($"Processing {oCurrentJob.JobTitle}");
 
-                    bool isSuccess = true;
-                    string result = "";
-                    JobTypeInfo oJobInfo = oController.GetJobTypeById(oCurrentJob.JobTypeId);
+                    var isSuccess = true;
+                    var result = "";
+                    var oJobInfo = oController.GetJobTypeById(oCurrentJob.JobTypeId);
                     try
                     {
-                        int rows = oController.ExecuteJob(oCurrentJob.JobScript, oJobInfo.IsCannedJob);
-                        result = "Success<br />" + rows.ToString() + " rows affected";
+                        var rows = oController.ExecuteJob(oCurrentJob.JobScript, oJobInfo.IsCannedJob);
+                        result =$"Success<br />{rows} rows affected";
                     }
                     catch (Exception ex)
                     {
                         isSuccess = false;
-                        result = "Error: " + ex.Message;
+                        result = $"Error: {ex.Message}";
                     }
 
                     //Update the entry and insert history
